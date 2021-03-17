27d1351c521df536fffdcad19ce5abe38beae0c0
Shrestha Upadhyay <shresthau19.96@gmail.com>
2021-03-17 11:33:40 +0530

    initial commit

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+
+    CleverTap.onUserLogin({ 'Name': 'ShreCordova', 'Identity': '666666', 'Email': 'shre@g.com', 'custom1': 456 }); //added by jidesh doc
+    CleverTap.setPushToken("1055953925629");
+    CleverTap.createNotificationChannel("123", "CleverTap", "This is a test channel", 5, true);
+}
